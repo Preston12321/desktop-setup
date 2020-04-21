0e5a2e546bb0e2d0aef58ca70931c836a9767241
Preston Locke <locke_preston@yahoo.com>
2020-04-21 16:48:19 -0500

    Updated install script

--- install-programs.sh
+++ install-programs.sh
@@ -1,38 +1,56 @@
 #!/bin/sh
 
 # Miscellaneous
-apt install apt-transport-https
 apt install net-tools
 apt install moreutils
-apt install wine64
 apt install xterm
 apt install vim
+apt install wine64
 apt install ttf-mscorefonts-installer
-
-# GNOME customization
 apt install chrome-gnome-shell
+
+# Plata theme for GNOME
+add-apt-repository ppa:tista/plata-theme
+apt update
 apt install plata-theme
 
-# Development tools
+# Development tools (apt)
+apt install build-essential
 apt install git
 apt install default-jre
 apt install adb
 apt install clang-format
 apt install python-pip
-apt install dart
 apt install gitk
-apt install mono-complete
 apt install cmake
-snap install android-studio
-snap install clion
+
+# Dart
+apt install apt-transport-https
+sh -c 'wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -'
+sh -c 'wget -qO- https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list'
+apt update
+apt install dart
+echo '# Add Dart to path variable' >> ~/.bashrc
+echo 'export PATH="$PATH:/usr/lib/dart/bin"' >> ~/.bashrc
+
+# Mono
+apt install gnupg ca-certificates
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+apt update
+apt install mono-complete
+
+# Development tools (snap)
 snap install code
 snap install hugo
 snap install postman
-snap install pycharm-professional
+snap install clion
+snap install android-studio
 
 # Media/Productivity
 snap install discord
 snap install inkscape
+snap install gimp
 snap install onlyoffice-desktopeditors
 snap install slack
 snap install spotify
